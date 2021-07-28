88bb86a7026682142e5683af50c00bf410644f84
iZboxo <61621463+iZboxo@users.noreply.github.com>
2021-07-28 12:03:19 +0200

    Add files via upload

---
+++ Part #4 - GCommands/index.js
@@ -0,0 +1,32 @@
+bconst { GCommands } = require("gcommands");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    const GCommandsClient = new GCommands(client, {
+        cmdDir: "commands/",
+        eventDir: "events/",
+        unkownCommandMessage: false, // true of false | send unkownCommand Message
+        language: "english", //english, spanish, portuguese, russian, german, czech, turkish
+        slash: {
+           slash: 'both', //true = slash only, false = only normal, both = slash and normal
+           prefix: '.' // for normal commands
+        },
+        defaultCooldown: 3,
+        database: "URL to mongodb"
+        /* DB SUPPORT
+         * redis://user:pass@localhost:6379
+         * mongodb://user:pass@localhost:27017/dbname
+         * sqlite://path/to/database.sqlite
+         * postgresql://user:pass@localhost:5432/dbname
+         * mysql://user:pass@localhost:3306/dbname
+        */
+    })
+    
+    GCommandsClient.on("log", (log) => {console.log(log)})
+
+    console.log("Ready")
+})
+
+client.login("Token Bot Here")
